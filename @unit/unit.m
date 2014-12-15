08c60a42dfb650aea12d083fb8aed7e393729369
Mark Mikofski <mark.mikofski@sunpowercorp.com>
2014-12-14 23:19:26 -0800

    add parsing static functions to unit

--- @unit/unit.m
+++ @unit/unit.m
@@ -4,14 +4,18 @@ classdef unit < double
         dimensionality % dimensionality
         value % SI equivalent value
         aliases
+        degree
+        bases
+        degrees = 1
     end
     methods
-        function u = unit(name,dimensionality,value,aliases)
+        function u = unit(name,dimensionality,value,aliases,bases,degrees)
             u = u@double(value);
             u.name = name;
             u.dimensionality = dimensionality;
             u.value = value;
             u.aliases = aliases;
+            
         end
         function disp(u)
             F = sprintf('%s [%s] =\n%s',u.name,u.dimensionality,u.value.to_string);
@@ -58,7 +62,7 @@ classdef unit < double
         function F = is_same_dimensionality(u,v)
             if isscalar(u) && isscalar(v)
                 F = strcmp(u.dimensionality,v.dimensionality);
-            elseif isscalaar(u)
+            elseif isscalar(u)
                 vdims = cellfun(@(x)x.dimensionality,v,'UniformOutput',false);
                 F = strcmp(u.dimensionality,vdims);
             end
@@ -69,10 +73,74 @@ classdef unit < double
                 F = x;
             end
         end
-        function F = times(x,y)
-            if x.is_same_dimensionality(y)
-                F = times(x.value,y);
+        function F = times(u,v)
+            % element-by-element array multiplication
+            %% y is double, make new quantity with units
+            if isa(u,'Quantities.unit') &&...
+                    ~(isa(v,'Quantities.unit') || isa(v,'Quantities.quantity'))
+                F = Quantities.quantity(v,0,u.name);
+            elseif isa(v,'Quantities.unit') &&...
+                    ~(isa(u,'Quantities.unit') || isa(u,'Quantities.quantity'))
+                F = Quantities.quantity(u,0,v.name);
+            elseif isa(u,'Quantities.unit') && isa(v,'Quantities.unit')
+                F = Quantities.unit();
             end
+        end
+    end
+    methods (Static)
+        function [basename,subexps] = parse_parentheses(u)
+            % find parenthetic subexpressions
+            basename = u;
+            tks = {''};
+            subexps = {};
+            while ~isempty(tks)
+                [tks,starts,ends] = regexp(basename,'\(([\w +-*/^]*)\)',...
+                    'tokens','start','end');
+                subexps = [subexps,tks{1}]; %#ok<AGROW>
+                for n = 1:numel(tks)
+                    basename(starts(n):ends(n)) = ['@',num2str(n)];
+                end
+            end
+        end
+        function [bases, degrees] = parse_dimensions(u)
+            % find dimensionality
+            [matches,splits] = regexp(u,'[*/]*','match','split');
+            if isempty(splits{1})
+                bases = {};
+                degrees = [];
+                return
+            else
+                sz = size(splits);
+                bases = cell(sz);
+                degrees = ones(sz);
+                tks = regexp(splits{1},'(\w+)\^((?<=\^)[.+-\d]+)','tokens');
+                if ~isempty(tks)
+                    bases(1) = tks{1}(1);
+                    degrees(1) = str2double(tks{2}{1});
+                else
+                    bases(1) = splits(1);
+                end
+            end
+            for n = 1:numel(matches)
+                switch matches(n)
+                    case '*'
+                        numerator_denominator = 1;
+                    case '/'
+                        numerator_denominator = -1;
+                end
+                tks = regexp(splits{n+1},'(\w+)\^((?<=\^)[.+-\d]+)','tokens');
+                if ~isempty(tks)
+                    bases(n+1) = tks{1}(1);
+                    degrees(n+1) = str2double(tks{2}{1})*numerator_denominator;
+                else
+                    bases(n+1) = splits(n+1);
+                    degrees(n+1) = numerator_denominator;
+                end
+            end
+        end
+        function [bases, degrees] = parse_name(u)
+            % parse units into base units and their degrees
+            [basename,subexps] = Quantities.unit.parse_parentheses(u);
         end
     end
 end
