45b23dca44e5b3913868605ac8990508d4fd3f7c
Mark Mikofski <mark.mikofski@sunpowercorp.com>
2014-12-18 23:32:22 -0800

    make stdev optional input, change variance to parameter input
    * variance overrides stdev if both provided, no warning
    * fix propagated variance for division and subtraction terms added since
     (-1)^2 = 1, duh!
    * in to_base() check if already in base units, then return input as output

--- @quantity/quantity.m
+++ @quantity/quantity.m
@@ -12,19 +12,28 @@ classdef quantity < double
             p.addRequired('average',...
                 @(arg)validateattributes(arg,{'numeric'},{'real','finite'},...
                 'quantity','average'));
-            p.addOptional('variance',zeros(size(average)),...
+            p.addOptional('stdev',zeros(size(average)),...
                 @(arg)validateattributes(arg,{'numeric'},...
                 {'real','finite','size',size(average),'nonnegative'},...
-                'quantity','variance'))
+                'quantity','stdev'))
             p.addOptional('units',Quantities.unit.DIMENSIONLESS,...
                 @(arg)validateattributes(arg,{'Quantities.unit'},{'scalar'},...
                 'quantity','units'))
+            p.addParameter('variance',zeros(size(average)),...
+                @(arg)validateattributes(arg,{'numeric'},...
+                {'real','finite','size',size(average),'nonnegative'},...
+                'quantity','variance'))
             p.parse(average,varargin{:});
             args = p.Results;
             x = x@double(args.average); % required for subclass of double
             x.average = double(x);
-            x.variance = double(args.variance); % cast numeric as double
-            x.stdev = sqrt(x.variance);
+            if strcmp('variance',p.UsingDefaults)
+                x.stdev = double(args.stdev); % cast numeric as double
+                x.variance = x.stdev.^2;
+            else
+                x.variance = double(args.variance); % cast numeric as double
+                x.stdev = sqrt(x.variance);
+            end
             x.relative = x.stdev./x.average;
             x.units = args.units;
         end
@@ -71,11 +80,11 @@ classdef quantity < double
         end
         function F = transpose(x)
             F = Quantities.quantity(transpose(x.average),...
-                transpose(x.variance),x.units);
+                transpose(x.stdev),x.units);
         end
         function F = ctranspose(x)
             F = Quantities.quantity(ctranspose(x.average),...
-                ctranspose(x.variance),x.units);
+                ctranspose(x.stdev),x.units);
         end
         function F = subsref(x,s)
             switch s(1).type
@@ -83,7 +92,7 @@ classdef quantity < double
                     F = subsref@double(x,s);
                 case '()'
                     F = Quantities.quantity(subsref(x.average,s(1)),...
-                        subsref(x.variance,s(1)),x.units);
+                        subsref(x.stdev,s(1)),x.units);
                     if numel(s)>1
                         F = subsref@double(F,s(2:end));
                     end
@@ -101,28 +110,28 @@ classdef quantity < double
                             'quantity:dimensionalMismatch',...
                             'Subscript assignment units must be same dimensionality.')
                         F = Quantities.quantity(subsasgn(x.average,s,y.average),...
-                            subsasgn(x.variance,s,y.variance),...
+                            subsasgn(x.stdev,s,y.stdev),...
                             x.units);
                     end
             end
         end
         function F = horzcat(varargin)
             avg_ = cellfun(@(x)x.average,varargin,'UniformOutput',false);
-            var_ = cellfun(@(x)x.variance,varargin,'UniformOutput',false);
+            stdev_ = cellfun(@(x)x.stdev,varargin,'UniformOutput',false);
             units_ = cellfun(@(x)x.units,varargin,'UniformOutput',false);
-            assert(units_{1}>=[units_{2:end}],...
+            assert(units_{1}>=units_(2:end),...
                 'quantity:dimensionalMismatch',...
                 'Horizontal concatenation units must be same dimensionality.')
-            F = Quantities.quantity([avg_{:}],[var_{:}],units_{1});
+            F = Quantities.quantity([avg_{:}],[stdev_{:}],units_{1});
         end
         function F = vertcat(varargin)
             avg_ = cellfun(@(x)x.average,varargin,'UniformOutput',false);
-            var_ = cellfun(@(x)x.variance,varargin,'UniformOutput',false);
+            stdev_ = cellfun(@(x)x.stdev,varargin,'UniformOutput',false);
             units_ = cellfun(@(x)x.units,varargin,'UniformOutput',false);
-            assert(units_{1}>=[units_{2:end}],...
+            assert(units_{1}>=units_(2:end),...
                 'quantity:dimensionalMismatch',...
                 'Vertical concatenation units must be same dimensionality.')
-            F = Quantities.quantity(vertcat(avg_{:}),vertcat(var_{:}),...
+            F = Quantities.quantity(vertcat(avg_{:}),vertcat(stdev_{:}),...
                 units_{1});
         end
         function F = cat(dim,varargin)
@@ -134,123 +143,127 @@ classdef quantity < double
         end
         function F = plus(x,y)
             % F = x+y
-            % dF^2 = dx.^2+dy.^2
+            % dF^2 = (1*dx).^2+(1*dy).^2
             x = Quantities.quantity.as_quantity(x);
             y = Quantities.quantity.as_quantity(y);
-            unit = x.units+y.units; % FIXME!
-            F = Quantities.quantity(plus@double(x,y),...
-                x.variance+y.variance,unit);
+            unit = x.units+y.units; % combine units
+            F = Quantities.quantity(plus@double(x,y),'units',unit,...
+                'variance',x.variance+y.variance);
         end
         function F = minus(x,y)
             % F = x-y
-            % dF^2 = dx.^2-dy.^2
+            % dF^2 = (1*dx).^2+(-1*dy).^2
             x = Quantities.quantity.as_quantity(x);
             y = Quantities.quantity.as_quantity(y);
-            unit = x.units-y.units; % FIXME!
-            F = Quantities.quantity(minus@double(x,y),...
-                x.variance-y.variance,unit);
+            unit = x.units-y.units; % combine units
+            F = Quantities.quantity(minus@double(x,y),'units',unit,...
+                'variance',x.variance+y.variance); % (-1)^2 = 1
         end
         function F = times(x,y)
             % F = x.*y
-            % dF^2 = y.^2.*dx.^2+x.^2.*dy.^2
+            % dF^2 = (y.*dx).^2+(x.*dy).^2
             x = Quantities.quantity.as_quantity(x);
             y = Quantities.quantity.as_quantity(y);
-            unit = x.units.*y.units; % FIXME!
-            F = Quantities.quantity(times@double(x,y),...
-                y.average.^2.*x.variance+x.average.^2.*y.variance,unit);
+            unit = x.units.*y.units; % combine units
+            F = Quantities.quantity(times@double(x,y),'units',unit,'variance',...
+                y.average.^2.*x.variance+x.average.^2.*y.variance);
         end
         function F = mtimes(x,y)
             % F_ik = x_ij*y_jk = x_i1*y_1k + x_i2*y_2k + x_i3*y_3k
             % dF_ik^2 = dx_ij.^2*y_jk.^2+x_ij.^2*dy_jk.^2
-            function dF = Fvar(x,y,dx,dy)
-                dF = zeros(size(x,1),size(y,2));
-                for idx = 1:size(x,1)
-                    for kdx = 1:size(y,2)
-                        dF(idx,kdx) = dx(idx,:).^2*y(:,kdx).^2+...
-                            x(idx,:).^2*dy(:,kdx).^2;
+            function dF = Fvar(xavg,yavg,xvariance,yvariance)
+                dF = zeros(size(xavg,1),size(yavg,2));
+                for idx = 1:size(xavg,1)
+                    for kdx = 1:size(yavg,2)
+                        dF(idx,kdx) = xvariance(idx,:)*yavg(:,kdx).^2+...
+                            xavg(idx,:).^2*yvariance(:,kdx);
                     end
                 end
             end
             x = Quantities.quantity.as_quantity(x);
             y = Quantities.quantity.as_quantity(y);
-            unit = x.units*y.units; % FIXME!
-            F = Quantities.quantity(mtimes@double(x,y),...
-                Fvar(x.average,y.average,x.stdev,y.stdev),unit);
+            unit = x.units*y.units; % combine units
+            F = Quantities.quantity(mtimes@double(x,y),'units',unit,...
+                'variance',Fvar(x.average,y.average,x.variance,y.variance));
         end
         function F = power(x,y)
             % F = x.^y
-            % dF^2 = (y.*x.^(y-1)).^2.*dx.^2+(log(x).*x.^y).^2.*dy.^2)
+            % dF^2 = (y.*x.^(y-1).*dx).^2+(log(x).*x.^y.*dy).^2)
             x = Quantities.quantity.as_quantity(x);
             y = Quantities.quantity.as_quantity(y);
-            unit = x.units.^y.units; % FIXME!
-            F = Quantities.quantity(power@double(x,y),...
+            unit = x.units.^y.units; % combine units
+            F = Quantities.quantity(power@double(x,y),'units',unit,'variance',...
                 (y.average.*x.average.^(y.average-1)).^2.*x.variance+...
-                (log(x.average).*x.average.^y.average).^2.*y.variance,unit);
+                (log(x.average).*x.average.^y.average).^2.*y.variance);
         end
         function F = rdivide(x,y)
             % F = x./y
-            % dF^2 = 1./y.^2.*dx.^2-x.^2./y.^4.*dy.^2
+            % dF^2 = (1./y.*dx).^2+(-x./y.^2.*dy).^2
             x = Quantities.quantity.as_quantity(x);
             y = Quantities.quantity.as_quantity(y);
-            unit = x.units./y.units; % FIXME!
-            F = Quantities.quantity(rdivide@double(x,y),...
-                1./y.average.^2.*x.variance-...
-                x.average.^2./y.average.^4.*y.variance,unit);
+            unit = x.units./y.units; % combine units
+            F = Quantities.quantity(rdivide@double(x,y),'units',unit,...
+                'variance',1./y.average.^2.*x.variance+...
+                (x.average./y.average.^2).^2.*y.variance); % (-1)^2 = 1
         end
         function F = ldivide(x,y)
             % F = x.\y = y./x
-            % dF^2 = 1./x.^2.*dy.^2-y.^2./x.^4.*dx.^2
+            % dF^2 = (1./x.*dy).^2+(-y./x.^2.*dx).^2
             x = Quantities.quantity.as_quantity(x);
             y = Quantities.quantity.as_quantity(y);
-            unit = x.units.\y.units; % FIXME!
-            F = Quantities.quantity(ldivide@double(x,y),...
-                1./x.average.^2.*y.variance-...
-                y.average.^2./x.average.^4.*x.variance,unit);
+            unit = x.units.\y.units; % combine units
+            F = Quantities.quantity(ldivide@double(x,y),'units',unit,...
+                'variance',1./x.average.^2.*y.variance+...
+                (y.average./x.average.^2).^2.*x.variance); % (-1)^2 = 1
         end
         function F = uplus(x)
             % F = +x
-            F = Quantities.quantity(uplus@double(x),x.variance,x.units);
+            F = Quantities.quantity(uplus@double(x),x.stdev,x.units);
         end
         function F = uminus(x)
             % F = -x
-            F = Quantities.quantity(uminus@double(x),x.variance,x.units);
+            F = Quantities.quantity(uminus@double(x),x.stdev,x.units);
         end
         function F = sin(x)
             % F = sin(x)
-            % dF^2 = cos(x)^2*dx^2
-            F = Quantities.quantity(sin@double(x),...
-                cos(x.average).^2.*x.variance.^2,x.units);
+            % dF^2 = (cos(x).*dx).^2
+            F = Quantities.quantity(sin@double(x),'units',x.units,...
+                'variance',cos(x.average).^2.*x.variance);
         end
         function F = cos(x)
             % F = cos(x)
-            % dF^2 = -sin(x)^2*dx^2
-            F = Quantities.quantity(cos@double(x),...
-                (-sin(x.average)).^2.*x.variance.^2,x.units);
+            % dF^2 = (-sin(x).*dx).^2
+            F = Quantities.quantity(cos@double(x),'units',x.units,...
+                'variance',sin(x.average).^2.*x.variance); % (-1)^2 = 1
         end
         function F = log(x)
             % F = log(x)
-            % dF^2 = (1/x)^2*dx^2
-            F = Quantities.quantity(log@double(x),...
-                1./x.average.^2.*x.variance.^2,x.units);
+            % dF^2 = (1/x.*dx).^2
+            F = Quantities.quantity(log@double(x),'units',x.units,...
+                'variance',1./x.average.^2.*x.variance);
         end
         function F = log2(x)
             % F = log(x)
-            % dF^2 = (1/x/log(2))^2*dx^2
-            F = Quantities.quantity(log2@double(x),...
-                1./(log(2)*x.average).^2.*x.variance.^2,x.units);
+            % dF^2 = (1/x/log(2).*dx).^2
+            F = Quantities.quantity(log2@double(x),'units',x.units,...
+                'variance',1./(log(2)*x.average).^2.*x.variance);
         end
         function F = log10(x)
             % F = log(x)
-            % dF^2 = (1/x/log(10))^2*dx^2
-            F = Quantities.quantity(log10@double(x),...
-                1./(log(10)*x.average).^2.*x.variance.^2,x.units);
+            % dF^2 = (1/x/log(10).*dx).^2
+            F = Quantities.quantity(log10@double(x),'units',x.units,...
+                'variance',1./(log(10)*x.average).^2.*x.variance);
         end
         function F = to_base(x)
+            if x.units.value==1
+                F = x;
+                return
+            end
             F = x.units.value.*x./x.units;
             F = F.combine_units;
         end
         function F = combine_units(x)
-            F = Quantities.quantity(x.average,x.variance,x.units.combine);
+            F = Quantities.quantity(x.average,x.stdev,x.units.combine);
         end
     end
     methods (Static)
