b485e3f176ffe5e7415becb0caf47626297373a3
Mark Mikofski <mark.mikofski@sunpowercorp.com>
2015-01-02 00:10:52 -0800

    add prefix and dimension classdefs

---
+++ prefix.m
@@ -0,0 +1,13 @@
+classdef prefix
+    properties
+        name
+        value
+        alias
+    end
+    methods
+        function pre = prefix(name,value)
+            pre.name = name;
+
+        end
+    end
+end
